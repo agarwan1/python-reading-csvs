8294859a3d8d8128a3c524ed168d7aa52215ca17
agarwan1 <agarwan1192@gmail.com>
2018-02-20 18:16:02 -0500

    Made changes with formatting and adding raw files.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,9 @@
 import os
-import re
 import csv
+
 csvpath = os.path.join('PyBankFile','budget_data_1.csv')
 csvpath2 = os.path.join('PyBankFile','budget_data_2.csv')
+
 files = [csvpath, csvpath2]
 ofile1 = "budgetdata1.csv"
 ofile2 = "budgetdata2.csv"
@@ -10,6 +11,7 @@ ofile = [ofile1, ofile2]
 
 for csvpath in files:
     rowcnt = 0
+
     with open(csvpath, newline = '') as csvfile:
         output_file = os.path.join(ofile[files.index(csvpath)])
         with open(output_file,'w',newline='') as data_file:
@@ -20,42 +22,55 @@ for csvpath in files:
             #print(csvreader)
             #row = []
 
+#Initializing all variabes.
             totalrevenue = 0
+            totalRevenueChange = 0
             revenue = 0
+            rowcnt = 0
             revenuechange = 0
             prevRev = 0
             maxrevchange = 0
             minrevchange = 0
             maxrevmonth = ""
             minrevmonth = ""
+            avgrevenue = 0
+            avgrevchange = 0
+#Looping through rows.
             for row in csvreader:
-                month = row[0] #NEED TO COUNT
-                revenue = row[1] #NEED TO SUM/AVERAGE
+                month = row[0]
+                revenue = row[1]
                 rowcnt = rowcnt + 1 #This is the number of months.
                 totalrevenue = float(totalrevenue) + float(row[1])
-                revenuechange = int(revenue) - int(prevRev)
+#I don't want to include the first row with the data. Revenue change needs to start from second month.
+                if rowcnt > 1:
+                    revenuechange = int(revenue) - int(prevRev)
+                    totalRevenueChange = totalRevenueChange + revenuechange
+#I don't want to skip the first row of data for the previous revenue.
                 prevRev = revenue
+#Compare this row's revenue change against the maximum revchange. Only keep maximum revenue change.
                 if float(revenuechange) > float(maxrevchange):
                     maxrevchange = revenuechange
                     maxrevmonth = month
+#Compare this row's revenue change against the minimum revchange. Only keep minimum revenue change.
                 if float(revenuechange) < float(minrevchange):
                     minrevchange = revenuechange
                     minrevmonth = month
-            avgrevenue = totalrevenue / rowcnt
-            avgrevchange = revenuechange / rowcnt
+#Calculate average revenue and average revenue change.
+            avgrevenue = totalrevenue / rowcnt - 1
+            avgrevchange = round((totalRevenueChange / (rowcnt - 1)), 2)
+#Print the results.
             print("Financial Analysis:" )
-            print("Total Months:"+str(rowcnt))
-            print(rowcnt)
-            print("Total Revenue:" + str(totalrevenue))
+            print("----------------------------")
+            print("Total Months: "+str(rowcnt))
+            #print(rowcnt)
+            print("Total Revenue: " + str(totalrevenue))
             #print(totalrevenue)
-            avgrevenue = totalrevenue / rowcnt
-            print("Average Revenue:" + str(avgrevenue))
-            #print(avgrevenue)
-            print("Average Revenue Change" + str(avgrevchange))
-            print("")
+            #avgrevenue = totalrevenue / rowcnt
+            #print("Average Revenue: " + str(avgrevenue))
+            print("Average Revenue Change: " + str(avgrevchange))
             print("Greatest Revenue Change: " + str(maxrevchange))
             print("Greatest Decrease Revenue Change: " + str(minrevchange))
-
+            print("")
 
 
 
--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,19 +1,15 @@
 import os
-#import re
 import csv
+
 csvpath = os.path.join('File','employee_data1.csv')
 csvpath2 = os.path.join('File','employee_data2.csv')
+
 files = [csvpath, csvpath2]
 ofile1 = "empdata1.csv"
 ofile2 = "empdata2.csv"
 ofile = [ofile1, ofile2]
-#import csv
-# empid = []
-# firstname = []
-# lastname = []
-# dob = []
-# ssn = []
-# state = []
+
+#US states dictionary.
 us_state_abbrev = {
     'Alabama': 'AL',
     'Alaska': 'AK',
@@ -66,6 +62,7 @@ us_state_abbrev = {
     'Wisconsin': 'WI',
     'Wyoming': 'WY',
 }
+
 for csvpath in files:
     with open(csvpath, newline = '') as csvfile:
         output_file = os.path.join(ofile[files.index(csvpath)])
@@ -74,23 +71,26 @@ for csvpath in files:
             output_writer.writerow(["Employee","First Name", "Last Name","DOB", "SSN", "State"])
             csvfile.readline()
             csvreader = csv.reader(csvfile, delimiter = ',')
-            #print(csvreader)
-            #row = []
+#Format the name, date of birth, social security number and state.
             for row in csvreader:
                 empid = row[0]
                 parsename = row[1].split(" ")
                 firstname = parsename[0]
                 lastname = parsename[1]
+#Format date to mm/dd/yyyy.
                 parsedob = row[2].split("-")
                 year = parsedob[0]
                 month = parsedob[1]
                 date = parsedob[2]
                 dob = month + "/" + date + "/" + year
+#Format scoail security number.
                 parsessn = row[3].split("-")
                 ssn = parsessn[2]
                 ssn = "***-***" + ssn
+#Format state.
                 stateAbbrv = us_state_abbrev.get(row[4])
                 state = stateAbbrv
+#Print formatted csv.
                 cleaned_csv = [empid, firstname, lastname, dob, ssn, state]
                 print(cleaned_csv)
                 output_writer.writerow(cleaned_csv)
